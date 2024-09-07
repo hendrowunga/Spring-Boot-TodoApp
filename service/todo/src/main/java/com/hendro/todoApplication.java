46921cd857b5f8a706e9390e6c6d1664acc91ff3
Hendro <hendrowunga073@gmail.com>
2024-09-08 01:09:10 +0700

    todo

--- service/todo/src/main/java/com/hendro/todoApplication.java
+++ service/todo/src/main/java/com/hendro/todoApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
-	}
-
+public class todoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(todoApplication.class, args);
+    }
 }
